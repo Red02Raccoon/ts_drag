672b9b1cf8fcbe350a6c8f8cf1273afca3fc5074
Red02Raccoon <red@raccoon@gmail.com>
2022-07-15 15:06:39 +0300

    render project form

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+// classes are used for educational purposes;
+
+class ProjectForm {
+    formTemplate: HTMLTemplateElement;
+    rootElement: HTMLDivElement;
+    form: HTMLFormElement
+
+    constructor() {
+        this.formTemplate = document.getElementById('project-input') as HTMLTemplateElement;
+        this.rootElement = document.getElementById('app') as HTMLDivElement;
+
+        this.form = document.importNode(this.formTemplate.content, true).firstElementChild as HTMLFormElement;
+        this.form.id = 'user-input'
+
+        this.render();
+    }
+
+    private render() {
+        this.rootElement.insertAdjacentElement('afterbegin', this.form)
+    }
+}
+
+const projectForm = new ProjectForm();
