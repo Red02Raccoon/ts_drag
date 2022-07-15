ceb637791cc7688dee7866f4579cdc71af31b9f3
Red02Raccoon <red@raccoon@gmail.com>
2022-07-15 15:16:22 +0300

    add handler to submit form

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,44 @@
 class ProjectForm {
     formTemplate: HTMLTemplateElement;
     rootElement: HTMLDivElement;
-    form: HTMLFormElement
+    form: HTMLFormElement;
+
+    titleInput: HTMLInputElement;
+    descriptionInput: HTMLInputElement;
+    peopleInput: HTMLInputElement;
 
     constructor() {
-        this.formTemplate = document.getElementById('project-input') as HTMLTemplateElement;
-        this.rootElement = document.getElementById('app') as HTMLDivElement;
+        this.formTemplate = document.getElementById(
+            "project-input"
+        ) as HTMLTemplateElement;
+        this.rootElement = document.getElementById("app") as HTMLDivElement;
 
-        this.form = document.importNode(this.formTemplate.content, true).firstElementChild as HTMLFormElement;
-        this.form.id = 'user-input'
+        this.form = document.importNode(this.formTemplate.content, true)
+            .firstElementChild as HTMLFormElement;
+        this.form.id = "user-input";
 
+        this.titleInput = this.form.querySelector("#title") as HTMLInputElement;
+        this.descriptionInput = this.form.querySelector(
+            "#description"
+        ) as HTMLInputElement;
+        this.peopleInput = this.form.querySelector("#people") as HTMLInputElement;
+
+        this.configureForm();
         this.render();
     }
 
     private render() {
-        this.rootElement.insertAdjacentElement('afterbegin', this.form)
+        this.rootElement.insertAdjacentElement("afterbegin", this.form);
+    }
+
+    private handleSubmit(e: Event) {
+        e.preventDefault();
+
+        console.log(e);
+    }
+
+    private configureForm() {
+        this.form.addEventListener("submit", this.handleSubmit.bind(this));
     }
 }
 
